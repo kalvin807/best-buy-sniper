b880f1318a4ccd5c0687f3b6ef9e58ea79e7d90c
Calvin Leung <kalvin807@gmail.com>
2021-02-26 18:33:52 +0800

    bump dep versions and use chrome

--- src/driver/index.ts
+++ src/driver/index.ts
@@ -1,15 +1,15 @@
-import { firefox, Browser } from 'playwright';
+import { chromium, Browser } from 'playwright';
 
 let browser: Browser;
 
 export const createBrowser = async (): Promise<Browser> => {
   const options = {
-    headless: false,
+    headless: true,
     ignoreHTTPSErrors: true,
     defaultViewport: { width: 1920, height: 1080 }
   };
 
-  browser = await firefox.launch(options);
+  browser = await chromium.launch(options);
 
   return browser;
 };
